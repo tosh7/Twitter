53e32a5bb6cd81bb0d555c0559e04b7ff2c7454f
tosh7 <zlia.6.lj.425@gmail.com>
2018-09-06 14:17:52 +0900

    made a base architechture

---
+++ Twitter/Sources/Controllers/Profile/ProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileViewController.swift
+//  Twitter
+//
+//  Created by Satoshi Komatsu on 2018/09/06.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
