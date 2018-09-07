36614839abbf04d66662af9368338e2739fbd724
tosh7 <zlia.6.lj.425@gmail.com>
2018-09-07 13:12:01 +0900

    changed info.plist and AppDelegate

---
+++ Twitter/Sources/Controllers/Start/StartViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StartViewController.swift
+//  Twitter
+//
+//  Created by Satoshi Komatsu on 2018/09/07.
+//  Copyright © 2018年 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
