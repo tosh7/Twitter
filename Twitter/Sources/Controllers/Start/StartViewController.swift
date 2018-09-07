ff74770119ccea280faf578f4b7984aa6c65c75d
Satoshi Komatsu <satoshikomatsu@SatoshinoiMac.local>
2018-09-07 23:22:55 +0900

    push

--- Twitter/Sources/Controllers/Start/StartViewController.swift
+++ Twitter/Sources/Controllers/Start/StartViewController.swift
@@ -7,29 +7,15 @@
 //
 
 import UIKit
+import TwitterKit
 
 class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//       let composer = TWTRComposer()
+//        composer.setText("test")
+//        composer.show(from: UIViewController, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
