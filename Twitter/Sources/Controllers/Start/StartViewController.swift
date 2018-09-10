a6ee0a5b4b3dd67a5c2a6c81a4a499a9363e1f9a
tosh7 <zlia.6.lj.425@gmail.com>
2018-09-11 02:02:32 +0900

    got a json data

--- Twitter/Sources/Controllers/Start/StartViewController.swift
+++ Twitter/Sources/Controllers/Start/StartViewController.swift
@@ -29,8 +29,32 @@ class StartViewController: UIViewController {
         TWTRTwitter.sharedInstance().logIn(completion: { (session, error) in
             if (session != nil) {
                 print("signed in as \(String(describing: session?.userName))")
+                self.performSegue(withIdentifier: "toTimeLine", sender: nil)
+                
             } else {
                 print("error: \(String(describing: error?.localizedDescription))")
+            }
+            
+            var clientError: NSError?
+            
+            let apiClient = TWTRAPIClient(userID: session?.userID)
+            let request = apiClient.urlRequest(
+                withMethod: "GET",
+                urlString: "https://api.twitter.com/1.1/statuses/user_timeline.json",
+                parameters: [
+                    "user_id": session?.userID,
+                    "count": "1", // Intで10を渡すとエラーになる模様で、文字列にしてやる必要がある
+                ],
+                error: &clientError
+            )
+            
+            apiClient.sendTwitterRequest(request) { response, data, error in // NSURLResponse?, NSData?, NSError?
+                if let error = error {
+                    print(error.localizedDescription)
+                } else if let data = data, let json = String(data: data, encoding: .utf8) {
+                    print("")
+                    print(json)
+                }
             }
         })
 
@@ -39,18 +63,4 @@ class StartViewController: UIViewController {
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         return TWTRTwitter.sharedInstance().application(app, open: url, options: options)
     }
-    
-    func getEmailAdress() {
-//        let client = TWTRAPIClient.withCurrentUser()
-//
-//        client.requestEmail { email, error in
-//            if (email != nil) {
-//                print("signed in as \(session.userName)");
-//            } else {
-//                print("error: \(error.localizedDescription)");
-//            }
-//        }
-    }
-    
-    
 }
