4c970989ccea8b1113cc8ec2ae10e5ac0ebf4416
tosh7 <zlia.6.lj.425@gmail.com>
2021-12-31 14:40:42 +0900

    updated API Client class

--- Twitter/Sources/Models/APIClient/APIClient.swift
+++ Twitter/Sources/Models/APIClient/APIClient.swift
@@ -12,9 +12,9 @@ protocol APIClientDelegate {
 
 actor APIClient {
     private let conf: Configuration
-//    private let session: URLSession
-//    private let serializer
-//    private let delegate: APIClientDelegate
+    private let session: URLSession
+    private let serializer: Serializer
+    private let delegate: APIClientDelegate
     private let loader = DataLoader()
 
     struct Configuration {
@@ -37,10 +37,42 @@ actor APIClient {
         }
     }
 
+    /// Initializes the client with the given parameters.
+    ///
+    /// - paramater host: 
+    convenience init(host: String, _ configure: (inout APIClient.Configuration) -> Void = { _ in }) {
+        var configuration = Configuration(host: host)
+        configure(&configuration)
+        self.init(configuration: configuration)
+    }
+
     init(configuration: Configuration) {
         self.conf = configuration
-        let queue = OperationQueue()
-        queue.maxConcurrentOperationCount = 1
+        let queue = OperationQueue(maxConcurrentOperationCount: 1)
         let delegate = URLSessionProxyDelegate.make(loader: loader, delegate: configuration.sessionDelegate)
+        self.session = URLSession(configuration: configuration.sessionConfiguration, delegate: delegate, delegateQueue: queue)
+        self.delegate = configuration.delegate ?? DefaultAPIClientDelegate()
+        self.serializer = Serializer(decoder: configuration.decoder, encoder: configuration.encoder)
     }
 }
+
+enum APIError: Error, LocalizedError {
+    case unaccepetedStatusCode(Int)
+
+    var errorDescription: String? {
+        switch self {
+        case .unaccepetedStatusCode(let statusCode):
+            return "Response status code was unacceptable: \(statusCode)."
+        }
+    }
+}
+
+extension APIClientDelegate {
+    func client(_ client: APIClient, willSendRequest request: inout URLRequest) {}
+    func shouldClientRetry(_ client: APIClient, withError error: Error) async -> Bool { false }
+    func client(_ client: APIClient, didReceiveInvalidationResponse response: HTTPURLResponse, data: Data) -> Error {
+        APIError.unaccepetedStatusCode(response.statusCode)
+    }
+}
+
+private struct DefaultAPIClientDelegate: APIClientDelegate {}
