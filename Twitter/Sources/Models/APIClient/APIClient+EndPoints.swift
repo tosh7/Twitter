b86ebe1fc0b110ca35d8c3249bd155d2ac539981
tosh7 <zlia.6.lj.425@gmail.com>
2022-07-25 02:46:58 +0900

    modyfied apiclient extension

--- Twitter/Sources/Models/APIClient/APIClient+EndPoints.swift
+++ Twitter/Sources/Models/APIClient/APIClient+EndPoints.swift
@@ -3,9 +3,7 @@ import Foundation
 extension ApiClient {
     func getTweet(_ request: TweetsRequest, completion:  @escaping ((Result<Tweets, Error>) -> Void)) { makeGetRequest(request: request, completion: completion) }
     func getTimeline(_ request: TimelinesRequest, completion: @escaping ((Result<Timelines, Error>) -> Void)) { makeGetRequest(request: request, completion: completion) }
-    func getUser(_ request: UserRequest, completion: @escaping ((Result<User, Error>) -> Void)) {
-        makeGetRequest(request: request, completion: completion)
-    }
+    func getUser(_ request: UserRequest, completion: @escaping ((Result<User, Error>) -> Void)) { makeGetRequest(request: request, completion: completion) }
 }
 
 extension ApiClient {
