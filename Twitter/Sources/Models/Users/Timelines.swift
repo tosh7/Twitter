5da65d9ba8b44eac6cc7a62a8465dcf652ed69a1
tosh7 <zlia.6.lj.425@gmail.com>
2022-07-24 15:08:17 +0900

    Added Timelines struct

--- Twitter/Sources/Models/Users/Timelines.swift
+++ Twitter/Sources/Models/Users/Timelines.swift
@@ -8,4 +8,25 @@ struct TimelinesRequest: RequestType {
     let userName: String = "tosh_3"
 }
 
-struct Timelines: Codable {}
+struct Timelines: Codable {
+    var data: [TweetData]
+    var includes: Includes?
+
+    struct TweetData: Codable {
+        var id: String
+        var text: String
+        var created_at: String?
+        var author_id: String?
+    }
+
+    struct Includes: Codable {
+        var users: [User]
+
+        struct User: Codable {
+            var created_at: String
+            var name: String
+            var username: String
+            var id: String
+        }
+    }
+}
